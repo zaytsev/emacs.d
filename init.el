ceafa6d4cade0f055a4c9d22db977f36ccbb588b
Sergey Zaytsev <sergey@zaytseff.net>
2012-01-20 23:52:21 +0700

    Added multi-web-mode

--- init.el
+++ init.el
@@ -36,8 +36,8 @@
 
 ;; slime
 (eval-after-load "slime" 
-  '(progn (slime-setup '(slime-repl))	
-	(defun paredit-mode-enable () (paredit-mode 1))	
+  '(progn (slime-setup '(slime-repl))
+	(defun paredit-mode-enable () (paredit-mode 1))
 	(add-hook 'slime-mode-hook 'paredit-mode-enable)
 	(add-hook 'slime-repl-mode-hook 'paredit-mode-enable)
 	(setq slime-protocol-version 'ignore)))
@@ -45,3 +45,12 @@
 (add-to-list 'load-path "~/.emacs.d/slime")
 (require 'slime)
 (slime-setup)
+
+(add-to-list 'load-path "~/.emacs.d/multi-web-mode")
+(require 'multi-web-mode)
+(setq mweb-default-major-mode 'html-mode)
+(setq mweb-tags  '((php-mode "<\\?php\\|<\\? \\|<\\?=" "\\?>")
+                   (js-mode "<script +\\(type=\"text/javascript\"\\|language=\"javascript\"\\)[^>]*>" "</script>")
+                   (css-mode "<style +type=\"text/css\"[^>]*>" "</style>")))
+(setq mweb-filename-extensions '("php" "htm" "html" "ctp" "phtml" "php4" "php5"))
+(multi-web-global-mode 1)
